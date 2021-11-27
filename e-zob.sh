e970716599d91629a20f68c634016d4bdd9c31ae
Sorrow Calamity <56769073+WarHawkREVS@users.noreply.github.com>
2021-11-27 04:32:34 +0100

    Update e-zob.sh

--- e-zob.sh
+++ e-zob.sh
@@ -1,6 +1,6 @@
 #Dik&Bash script for fun in KOTH THM its a simple infinite loop x)
 #!/bin/bash
-for ((;;)
+for ((;;))
 do
   echo "8===========================D <----- dick and bash By Tetsu Calamîty"
 done
